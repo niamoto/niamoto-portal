573a04c18bbda1cd1c34a289e0c36fb973a6ef2a
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-04-21 17:20:57 +1100

    Add occurrence replacement task.

--- plantnote_io/tasks.py
+++ plantnote_io/tasks.py
@@ -5,9 +5,17 @@ from __future__ import absolute_import
 from celery import shared_task
 
 from ncbif_taxa.data_io import delete_all_taxa, import_taxon_from_plantnote_db
+from ncbif_occurrences.data_io import delete_all_occurrences, \
+    import_occurrences_from_plantnode_db
 
 
 @shared_task
 def replace_taxa_from_plantnote_db(plantnote_db):
     delete_all_taxa()
     import_taxon_from_plantnote_db(plantnote_db)
+
+
+@shared_task
+def replace_occurrences_from_plantnote_db(plantnote_db):
+    delete_all_occurrences()
+    import_occurrences_from_plantnode_db(plantnote_db)
