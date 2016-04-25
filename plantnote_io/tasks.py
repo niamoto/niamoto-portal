032a732d19d81c07c4f9852669efda43360fa591
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-04-25 15:49:35 +1100

    make whole plantnote db replacement atomic.

--- plantnote_io/tasks.py
+++ plantnote_io/tasks.py
@@ -2,20 +2,23 @@
 
 from __future__ import absolute_import
 
+from django.db import transaction
 from celery import shared_task
 
-from ncbif_taxa.data_io import delete_all_taxa, import_taxon_from_plantnote_db
-from ncbif_occurrences.data_io import delete_all_occurrences, \
-    import_occurrences_from_plantnode_db
+from ncbif_taxa import data_io as taxa_io
+from ncbif_occurrences import data_io as occurrences_io
 
 
 @shared_task
-def replace_taxa_from_plantnote_db(plantnote_db):
-    delete_all_taxa()
-    import_taxon_from_plantnote_db(plantnote_db)
-
-
-@shared_task
-def replace_occurrences_from_plantnote_db(plantnote_db):
-    delete_all_occurrences()
-    import_occurrences_from_plantnode_db(plantnote_db)
+def replace_plantnote_db(plantnote_db):
+    """
+    Replace the current plantnote db on which taxa and occurrences records
+    are based.
+    :param plantnote_db: The path to the plantnote db, previously converted to
+    sqlite.
+    """
+    with transaction.atomic():
+        occurrences_io.delete_all_occurrences()
+        taxa_io.delete_all_taxa()
+        taxa_io.import_taxon_from_plantnote_db(plantnote_db)
+        occurrences_io.import_occurrences_from_plantnode_db(plantnote_db)
