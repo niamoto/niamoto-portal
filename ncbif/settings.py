d6dd556cf58fa9fc5f2f1191524c10ac47702d76
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-04-26 14:40:59 +1100

    Add media_root/url and overwrite file storage for plantnote db model.

--- ncbif/settings.py
+++ ncbif/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = 'media/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
