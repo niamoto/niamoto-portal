1b47c38b9a329f9958bc2db08dbc554b290c48fb
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-06-10 17:35:30 +1100

    Start webportal with pinax bootstrap theme and pinax account

--- niamoto/urls.py
+++ niamoto/urls.py
@@ -48,6 +48,8 @@ urlpatterns = [
     url(r'^{}/'.format(settings.REST_API_BASE_URL), include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^account/', include('account.urls')),
+    url(r'^', include('web_portal.urls')),
 ]
 
 if settings.DEBUG is True:
