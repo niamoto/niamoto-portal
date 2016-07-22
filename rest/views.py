c0d5ddab511aa88a7e41917a1db1134736c87675
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-07-22 11:46:34 +1100

    Add only_team filter to user list

--- rest/views.py
+++ rest/views.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 
 from rest.serializers import UserSerializer
@@ -11,5 +11,11 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     ViewSet for retrieving niamoto's users.
     """
     base_name = 'user'
-    queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        only_team = self.request.query_params.get('only_team', None)
+        if only_team:
+            group = Group.objects.get(name='team')
+            return group.user_set.all()
+        return User.objects.filter(is_active=True)
