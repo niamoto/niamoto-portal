eb602c52f40b47715a0b74b636bbd33e03952a06
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-08-02 17:54:45 +1100

    fix again again plot sql

--- apps/niamoto_data/data_io/plot.py
+++ apps/niamoto_data/data_io/plot.py
@@ -61,7 +61,7 @@ def import_plots_from_plantnote_db(database):
         VALUES {};
         """.format(
             Plot._meta.db_table,
-            ','.join(["('{}', '{}', '{}', '{}', {})".format(
+            ','.join(["('{}', '{}', {}, {}, {})".format(
                 row[0],
                 get_width_row(row),
                 get_height_row(row),
