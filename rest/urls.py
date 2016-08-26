68c20818c4063ea6f28211e506521a5f2878d5d7
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-08-26 15:46:05 +1100

    Add api_root view

--- rest/urls.py
+++ rest/urls.py
@@ -1,18 +1,16 @@
 # coding: utf-8
 
 from django.conf.urls import url, include
-from rest_framework import routers
 
-
-router = routers.DefaultRouter()
+from rest.views import api_root
 
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^data/', include('apps.niamoto_data.rest_urls')),
-    url(r'^plantnote/', include('apps.niamoto_plantnote.rest_urls')),
-    url(r'^forest_digitizing/', include('apps.forest_digitizing.rest_urls')),
-    url(r'^rapid_inventory/', include('apps.rapid_inventories.rest_urls')),
-    url(r'^docs/', include('rest_framework_docs.urls')),
-    url(r'^management/', include('apps.niamoto_management.rest_urls')),
+    url(r'^$', api_root, name="api-root"),
+    url(r'^docs/', include('rest_framework_docs.urls', namespace="docs")),
+    url(r'^data/', include('apps.niamoto_data.rest_urls', namespace="data-api")),
+    url(r'^plantnote/', include('apps.niamoto_plantnote.rest_urls', namespace="plantnote-api")),
+    url(r'^forest_digitizing/', include('apps.forest_digitizing.rest_urls', namespace="forest_digitizing-api")),
+    url(r'^rapid_inventory/', include('apps.rapid_inventories.rest_urls', namespace="rapid_inventory-api")),
+    url(r'^management/', include('apps.niamoto_management.rest_urls', namespace="management-api")),
 ]
--- rest/views.py
+++ rest/views.py
@@ -1,1 +1,18 @@
 # coding: utf-8
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'docs': reverse("docs:drfdocs"),
+        'data-api': reverse('data-api:api-root'),
+        'plantnote-api': reverse('plantnote-api:api-root'),
+        'forest_digitizing-api': reverse('forest_digitizing-api:api-root'),
+        'rapid_inventory-api': reverse('rapid_inventory-api:api-root'),
+        'management-api': reverse('management-api:api-root'),
+    })
+
