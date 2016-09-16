9a25926f0a982715e39d2b1f40c3943f835d88f1
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-09-16 18:27:16 +1100

    New import code for occurrences ok

--- apps/niamoto_plantnote/data_io/occurrence.py
+++ apps/niamoto_plantnote/data_io/occurrence.py
@@ -3,11 +3,29 @@
 import sqlite3
 from datetime import date
 
-from django.db import connection
+from django.db import connection, transaction
 import numpy as np
+from numpy.lib.recfunctions import append_fields
 
 from apps.niamoto_data.models import Occurrence
 from ..models import PlantnoteOccurrence
+
+
+@transaction.atomic
+def import_occurrences_from_plantnote_db(database):
+    """
+    Import an occurrence list from a .ptx Pl@ntnote database, previously
+    converted to a sqlite database.
+    :param database: The path to the database.
+    """
+    plantnote_data = get_occurrences_from_plantnote_db(database)
+    niamoto_data = get_current_plantnote_occurrences()
+    insert_selection = get_insert_selection(plantnote_data, niamoto_data)
+    update_selection = get_update_selection(plantnote_data, niamoto_data)
+    delete_selection = get_delete_selection(plantnote_data, niamoto_data)
+    insert_occurrences(insert_selection)
+    update_occurrences(update_selection)
+    delete_occurrences(delete_selection)
 
 
 def get_occurrences_from_plantnote_db(database):
@@ -19,16 +37,17 @@ def get_occurrences_from_plantnote_db(database):
     sql = \
         """
         SELECT Indiv."ID Individus" AS id_indiv,
-               COALESCE(Inv."Date Inventaire", "null") AS date_obs,
-               COALESCE(Det."ID Taxons", "null") AS id_taxon,
+               COALESCE(Inv."Date Inventaire", 'null') AS date_obs,
+               COALESCE(Det."ID Taxons", 'null') AS id_taxon,
                Col."Collecteur" AS col_name,
-               "ST_GeomFromText('POINT(" || Loc.LongDD || " " || Loc.LatDD || "'), 4326)" AS location
+               'POINT(' || Loc.LongDD || ' ' || Loc.LatDD || ')' AS location
         FROM Individus AS Indiv
         INNER JOIN Inventaires AS Inv ON Indiv."ID Inventaires" = Inv."ID Inventaires"
         LEFT JOIN Localités AS Loc ON Inv."ID Parcelle" = Loc."ID Localités"
         LEFT JOIN Déterminations AS Det ON Indiv."ID Déterminations" = Det."ID Déterminations"
         LEFT JOIN Observations AS Obs ON Indiv."ID Observations" = Obs."ID Observations"
-        LEFT JOIN Collecteurs AS Col ON Obs."Observateur" = Col."ID Collecteurs";
+        LEFT JOIN Collecteurs AS Col ON Obs."Observateur" = Col."ID Collecteurs"
+        ORDER BY id_indiv;
         """
     conn = sqlite3.connect(database)
     cur = conn.cursor()
@@ -64,26 +83,27 @@ def get_current_plantnote_occurrences():
     """
     pg_sql = \
         """
-        SELECT o.id,
+        SELECT p.plantnote_id AS plantnote_id,
                o.date,
-               'ST_GeomFromText(''POINT(' || ST_X(o.location) || ' ' || ST_Y(o.location) || '''), 4326)' AS location,
-               o.taxon_id,
-               p.plantnote_id,
-               p.collector
+               COALESCE(o.taxon_id::text, 'null'),
+               p.collector,
+               'POINT(' || ST_X(o.location) || ' ' || ST_Y(o.location) || ')' AS location,
+               o.id
        FROM {} AS p
-       INNER JOIN {} AS o ON o.id = p.occurrence_ptr_id;
+       INNER JOIN {} AS o ON o.id = p.occurrence_ptr_id
+       ORDER BY plantnote_id;
         """.format(PlantnoteOccurrence._meta.db_table, Occurrence._meta.db_table)
     cursor = connection.cursor()
     cursor.execute(pg_sql)
     data = np.array(
         cursor.fetchall(),
         dtype=[
-            ('id', 'int'),
-            ('date_obs', 'U50'),
-            ('location', 'U300'),
-            ('tax_id', 'U50'),
             ('plantnote_id', 'int'),
+            ('date_obs', 'U50'),
+            ('tax_id', 'U50'),
             ('collector', 'U300'),
+            ('location', 'U300'),
+            ('id', 'int'),
         ]
     )
     return data
@@ -123,8 +143,11 @@ def get_update_selection(plantnote_data, niamoto_data):
     )
     a = plantnote_data[bool_diff_a]
     b = niamoto_data[bool_diff_b]
-    to_update = ~np.all(np.equal(a, b), axis=1)
-    return a[to_update]
+    a = append_fields(a, 'id', b['id'], dtypes=b['id'].dtype, usemask=False)
+    eq = a == b
+    print(a[:1])
+    print(b[:1])
+    return a[~eq]
 
 
 def get_delete_selection(plantnote_data, niamoto_data):
@@ -141,80 +164,104 @@ def get_delete_selection(plantnote_data, niamoto_data):
     return niamoto_data[bool_diff]
 
 
-def delete_all_occurrences():
-    pg_sql = \
-        """
-        DELETE FROM {}
-        """.format(Occurrence._meta.db_table)
-    cursor = connection.cursor()
-    cursor.execute(pg_sql)
-
-
-def import_occurrences_from_plantnote_db(database):
-    """
-    Import an occurrence list from a .ptx Pl@ntnote database, previously
-    converted to a sqlite database.
-    :param database: The path to the database.
-    """
+@transaction.atomic
+def insert_occurrences(insert_selection):
+    if len(insert_selection) == 0:
+        return
+    # Generate rows to insert in Occurrence base model
+    values_occ = []
+    for row in insert_selection:
+        values_occ.append("('{}', ST_GeomFromText('{}', 4326), {})".format(
+            row['date_obs'],
+            row['location'],
+            row['tax_id']
+        ))
+    # Insert records and returned assigned id's
     sql = \
         """
-        SELECT Indiv."ID Individus" AS id_indiv,
-               Inv."Date Inventaire" AS date_obs,
-               Det."ID Taxons" AS id_taxon,
-               Col."Collecteur" AS col_name,
-               Loc.LongDD,
-               Loc.LatDD
-        FROM Individus AS Indiv
-        INNER JOIN Inventaires AS Inv ON Indiv."ID Inventaires" = Inv."ID Inventaires"
-        LEFT JOIN Localités AS Loc ON Inv."ID Parcelle" = Loc."ID Localités"
-        LEFT JOIN Déterminations AS Det ON Indiv."ID Déterminations" = Det."ID Déterminations"
-        LEFT JOIN Observations AS Obs ON Indiv."ID Observations" = Obs."ID Observations"
-        LEFT JOIN Collecteurs AS Col ON Obs."Observateur" = Col."ID Collecteurs";
+        WITH occ AS (
+            INSERT INTO {} (date, location, taxon_id)
+            VALUES {}
+            RETURNING id
+        )
+        SELECT id FROM occ;
+        """.format(Occurrence._meta.db_table, ','.join(values_occ))
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    ids = cursor.fetchall()
+    # ids lenght should be equal to insert selection
+    assert len(ids) == len(insert_selection)
+    # Generate rows to insert in PlantnoteOccurrence extension model
+    values_plantnote_occ = []
+    for i, row in enumerate(insert_selection):
+        values_plantnote_occ.append("({}, {}, '{}')".format(
+            ids[i][0],
+            row['plantnote_id'],
+            row['collector'],
+        ))
+    # Insert records
+    sql_plantnote_occ = \
         """
-    conn = sqlite3.connect(database)
-    cur = conn.cursor()
-    cur.execute(sql)
-    data = cur.fetchall()
-
-    def get_location_col(row):
-        long = row[4]
-        lat = row[5]
-        if long is None or lat is None:
-            return "NULL"
-        else:
-            return "ST_GeomFromText('POINT({} {})', 4326)".format(long, lat)
-
-    def get_date_col(row):
-        date_str = row[1]
-        if len(date_str) > 0:
-            sd = date_str.split("/")
-            if len(sd) == 3:
-                d = date(*[int(i) for i in sd])
-            elif len(sd) == 2:
-                d = date(int(sd[0]), int(sd[1]), 1)
-            else:
-                d = date(int(sd[0]), 1, 1)
-        return d
-
-    def get_taxon_col(row):
-        id_taxon = row[2]
-        if id_taxon is None:
-            return "null"
-        return id_taxon
-
-    # Now process bulk insert on PG database
-    pg_sql = \
-        """
-        INSERT INTO {} (id, date, location, taxon_id)
+        INSERT INTO {} (occurrence_ptr_id, plantnote_id, collector)
         VALUES {};
         """.format(
-            Occurrence._meta.db_table,
-            ','.join(["('{}','{}',{}, {})".format(
-                row[0],
-                get_date_col(row),
-                get_location_col(row),
-                get_taxon_col(row),
-            ) for row in data])
+            PlantnoteOccurrence._meta.db_table,
+            ','.join(values_plantnote_occ)
         )
     cursor = connection.cursor()
-    cursor.execute(pg_sql)
+    cursor.execute(sql_plantnote_occ)
+
+
+@transaction.atomic
+def update_occurrences(update_selection):
+    if len(update_selection) == 0:
+        return
+    # Update records in Occurrence base model
+    updates = [
+        """
+        UPDATE {} SET date = '{}', location = ST_GeomFromText('{}', 4326), taxon_id = {}
+        WHERE id = {}
+        """ .format(
+            Occurrence._meta.db_table,
+            row['date_obs'],
+            row['location'],
+            row['tax_id'],
+            row['id']
+        )
+        for row in update_selection
+    ]
+    cursor = connection.cursor()
+    cursor.execute(';'.join(updates))
+    # Update records in PlantnoteOccurrence extension model
+    plantnote_updates = [
+        """
+        UPDATE {} SET collector = '{}'
+        WHERE occurrence_ptr_id  = {}
+        """.format(
+            PlantnoteOccurrence._meta.db_table,
+            row['collector'],
+            row['id']
+        )
+        for row in update_selection
+        ]
+    cursor = connection.cursor()
+    cursor.execute(';'.join(plantnote_updates))
+
+
+@transaction.atomic
+def delete_occurrences(delete_selection):
+    if len(delete_selection) == 0:
+        return
+    in_array = ','.join([str(i) for i in delete_selection['id']])
+    sql = \
+        """
+        DELETE FROM {} WHERE occurrence_ptr_id IN ({});
+        DELETE FROM {} WHERE id IN ({});
+        """.format(
+            PlantnoteOccurrence._meta.db_table,
+            in_array,
+            Occurrence._meta.db_table,
+            in_array
+        )
+    cursor = connection.cursor()
+    cursor.execute(sql)
