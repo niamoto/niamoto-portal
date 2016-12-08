e694146dfcda8812b043f3afac1980cdd5baece9
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-12-08 15:45:05 +1100

    Refactor inventory index with requirejs

--- apps/inventories/static/inventories/js/consult_inventories.js
+++ apps/inventories/static/inventories/js/consult_inventories.js
@@ -1,74 +1,35 @@
-(function($, undefined) {
+require([
+    'jquery',
+    'ol',
+    'utils/maps',
+    'jquery.tablesorter'
+], function($, ol, maps) {
 
-    // Add EPSG:32758 projection
-    proj4.defs(
-        "EPSG:32758",
-        "+proj=utm +zone=58 +south +datum=WGS84 +units=m +no_defs"
-    );
-
-    var wms_url = 'http://carto.gouv.nc/arcgis/services/fond_imagerie/MapServer/WMSServer';
-    var target = 'map';
-    var view = new ol.View({
-        projection: 'EPSG:4326',
-        center: new ol.proj.transform([165.875, -21.145],
-                                      'EPSG:4326',
-                                      'EPSG:4326'),
-        zoom: 7.5
-    });
+    var map = maps.getDefaultMap();
     var inventories_layer;
-    var map = new ol.Map({
-        target: target,
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.TileWMS({
-                    url: wms_url,
-                    params: {
-                        LAYERS: '0',
-                        FORMAT: 'image/png',
-                        CRS: 'EPSG:32758'
-                    },
-                    serverType: 'mapserver'
-                })
-            })
-        ],
-        view: view,
-        controls: [
-            new ol.control.Zoom(),
-        ]
-    });
+
+    function hidePreloader() {
+        document.getElementById('preloader').style.display = 'none';
+    };
 
     $(document).ready(function() {
         $("#inventories_table").tablesorter({
             dateFormat: "ddmmyyyy"
         });
         // Load data
-        var progress_bar = new olext.control.ProgressBar();
-        progress_bar.setLabel("0%");
-        map.addControl(progress_bar);
         $.ajax({
-            xhr: function() {
-                var xhr = new window.XMLHttpRequest();
-                xhr.addEventListener("progress", function(evt) {
-                    if (evt.lengthComputable) {
-                        var progr = (evt.loaded / evt.total * 100).toFixed(1);
-                        progress_bar.setValue(progr);
-                        progress_bar.setLabel(progr + "%");
-                    }
-                }, false);
-                return xhr;
-            },
             type: 'GET',
             url: rapid_inventory_url,
             success: function(result) {
               var inventories_geojson = result.results;
               addInventoriesLayer(inventories_geojson);
-              map.removeControl(progress_bar);
+              hidePreloader();
             }
         });
 
         function addInventoriesLayer(inventories_geojson) {
             // Create and add massif layer
-            inventories_layer = window.olext.utils.makeGeoJSONLayer(
+            inventories_layer = maps.makeGeoJSONLayer(
                 inventories_geojson, {
                     style: new ol.style.Style({
                         image: new ol.style.Circle({
@@ -106,7 +67,7 @@
         };
 
         function addPopupOverlay() {
-            var popup = new olext.overlay.Popup({
+            var popup = new maps.Popup({
                 'positioning': 'bottom-center',
                 'autoPan': true,
                 'autoPanAnimation': {
@@ -148,4 +109,4 @@
 
     });
 
-})(jQuery);
+});
