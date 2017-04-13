13f6fb81a42f76a45575c25e9d198ea9d27756bd
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-04-13 20:06:07 +0200

    Disable scheduled backup

--- niamoto/celeryconfig.py
+++ niamoto/celeryconfig.py
@@ -10,18 +10,18 @@ CELERY_TIMEZONE = settings.TIME_ZONE
 # Celery Schedule
 CELERYBEAT_SCHEDULE = {
     # Backup the database every first day of the month
-    'monthly-db-backup': {
-        'task': 'apps.niamoto_management.tasks.monthly_backup_db',
-        'schedule': crontab(0, 0, day_of_month='1')
-    },
+    # 'monthly-db-backup': {
+    #     'task': 'apps.niamoto_management.tasks.monthly_backup_db',
+    #     'schedule': crontab(0, 0, day_of_month='1')
+    # },
     # Backup the database every day at midnight
-    'daily-db-backup': {
-        'task': 'apps.niamoto_management.tasks.daily_backup_db',
-        'schedule': crontab(minute=0, hour=0)
-    },
+    # 'daily-db-backup': {
+    #     'task': 'apps.niamoto_management.tasks.daily_backup_db',
+    #     'schedule': crontab(minute=0, hour=0)
+    # },
     # Backup the database every hour
-    'hourly-db-backup': {
-        'task': 'apps.niamoto_management.tasks.hourly_backup_db',
-        'schedule': crontab(minute=0)
-    }
+    # 'hourly-db-backup': {
+    #     'task': 'apps.niamoto_management.tasks.hourly_backup_db',
+    #     'schedule': crontab(minute=0)
+    # }
 }
