09b29d2ec60a70d72614918c502836706f5061f4
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-07-08 12:41:54 +0200

    Design richness analysis form

--- js/data_marts/data_mart.js
+++ js/data_marts/data_mart.js
@@ -4,7 +4,10 @@ import ol from 'openlayers';
 import {
     Map, getDefaultMap
 } from '../niamoto_base_map';
-
+import {
+    form, Grid, Row, Col, FormGroup, ControlLabel, FormControl,
+    HelpBlock, option, Panel
+} from 'react-bootstrap';
 
 var map = getDefaultMap();
 var features = new ol.Collection();
@@ -27,9 +30,57 @@ map.addInteraction(draw);
 map.addInteraction(modify);
 
 
+function FieldGroup({ id, label, help, ...props }) {
+    return (
+        <FormGroup controlId={id}>
+          <ControlLabel>{label}</ControlLabel>
+          <FormControl {...props} />
+          {help && <HelpBlock>{help}</HelpBlock>}
+        </FormGroup>
+    );
+}
+
+
 function App() {
     return (
-        <Map map={map} target={'map'}/>
+        <Grid>
+          <Panel>
+            <Row>
+              <Col xs={6} md={4}>
+                <p>
+                  Sélectionnez une zone à analyser. Vous pouvez la
+                  dessiner sur la carte, charger un shapefile, ou bien
+                  sélectionner une entité dans les listes déroulantes.
+                </p><br/>
+                <form>
+                  <FieldGroup
+                    id="formControlsFile"
+                    type="file"
+                    label="Charger un shapefile"
+                  />
+                  <FormGroup controlId="formControlsSelect">
+                    <ControlLabel>Sélectionner un massif</ControlLabel>
+                    <FormControl componentClass="select" placeholder="select">
+                    </FormControl>
+                  </FormGroup>
+                  <FormGroup controlId="formControlsSelect">
+                    <ControlLabel>Sélectionner une province</ControlLabel>
+                    <FormControl componentClass="select" placeholder="select">
+                    </FormControl>
+                  </FormGroup>
+                  <FormGroup controlId="formControlsSelect">
+                    <ControlLabel>Sélectionner une commune</ControlLabel>
+                    <FormControl componentClass="select" placeholder="select">
+                    </FormControl>
+                  </FormGroup>
+                </form>
+              </Col>
+              <Col xs={6} md={8}>
+                <Map map={map} target={'map'}/>
+              </Col>
+            </Row>
+          </Panel>
+        </Grid>
     )
 }
 
