7629bd22db3573d4c3c937fb1acb2e25632b441c
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-07-18 04:31:19 +0200

    Make data mart dist dir

--- build_js.sh
+++ build_js.sh
@@ -15,4 +15,5 @@ node web/static/js/libs/r.js -o apps/inventories/static/inventories/js/jsbuild/b
 node web/static/js/libs/r.js -o apps/inventories/static/inventories/js/jsbuild/build_taxa_inventory.js
 
 # Build data marts js
+mkdir apps/data_marts/static/data_marts/css/js
 browserify js/data_marts/data_mart.js -t babelify --presets [es2015 react] -o apps/data_marts/static/data_marts/js/dist/data_mart.js
