3cd2c68c4e4de05e67bacf9e60708de160276727
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-12-15 15:20:29 +1100

    fix broken dependency to niamoto_plantnote

--- rest/views.py
+++ rest/views.py
@@ -15,7 +15,6 @@ def api_root(request):
         'data-api': reverse('data-api:api-root'),
         'dashboard-api': reverse('dashboard-api:api-root'),
         'data_mart-api': reverse('data_mart-api:api-root'),
-        'plantnote-api': reverse('plantnote-api:api-root'),
         'forest_digitizing-api': reverse('forest_digitizing-api:api-root'),
         'inventory-api': reverse('inventory-api:api-root'),
     }
