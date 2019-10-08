0c2ac716ad36a46fef9e0c2363506c267d06d17a
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-08 16:56:26 +1100

    config

--- niamoto_portal/settings.py
+++ niamoto_portal/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -28,7 +29,11 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+ADMINS = (
+    ('TM', 'tristan.mangeard@gmail.com'),
+)
 
 
 # Application definition
@@ -121,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'site_media', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'site_media', 'media')
 
 # Parameter for DEBUG
 if DEBUG:
