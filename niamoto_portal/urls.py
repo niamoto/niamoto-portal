80420cf81304d4f61c00b2ece88566e5889a59bb
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-11 10:48:32 +1100

    reformat file

--- niamoto_portal/urls.py
+++ niamoto_portal/urls.py
@@ -29,5 +29,7 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
