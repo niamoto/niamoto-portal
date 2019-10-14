e04a99ac35517f751d1da986a3d65dd890ba8277
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-15 07:56:04 +1100

    separe file config prod, heroku, travis

---
+++ niamoto_portal/settings/heroku.py
@@ -0,0 +1,14 @@
+import os
+from .base import *
+import django_heroku
+import dj_database_url
+
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
---
+++ niamoto_portal/settings/travis.py
@@ -0,0 +1,13 @@
+from base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.contrib.gis.db.backends.postgis',
+        'NAME':     'travisci',
+        'USER':     'postgres',
+        'PASSWORD': '',
+        'HOST':     'localhost',
+        'PORT':     '',
+    }
+}
