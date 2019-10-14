93b81c97bb420391f7ebfe70cc526df6fa8c336e
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-14 16:31:36 +1100

    hide settings heroku

--- niamoto_portal/settings.py
+++ niamoto_portal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -113,7 +113,7 @@ if 'TRAVIS' in os.environ:
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 
 # Password validation
