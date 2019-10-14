ddb2d290d9baa29c3fdd04b3b4e82ea90636633d
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-14 16:18:00 +1100

    add variable heroku database

--- niamoto_portal/settings.py
+++ niamoto_portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,6 +112,8 @@ if 'TRAVIS' in os.environ:
 # Heroku
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 
 # Password validation
