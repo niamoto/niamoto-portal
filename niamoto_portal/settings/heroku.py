8b9dd850224b61fc1a99e577b338293b77c21c73
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-15 12:59:09 +1100

    suspens django_heroku

--- niamoto_portal/settings/heroku.py
+++ niamoto_portal/settings/heroku.py
@@ -6,7 +6,7 @@ import dj_database_url
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
