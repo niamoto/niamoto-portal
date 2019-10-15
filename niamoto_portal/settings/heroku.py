78b176ed5a03dfe6a862eafff234d9f4e4a5f87b
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-15 15:23:14 +1100

    clean file

--- niamoto_portal/settings/heroku.py
+++ niamoto_portal/settings/heroku.py
@@ -7,8 +7,3 @@ GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
