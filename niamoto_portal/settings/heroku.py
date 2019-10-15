ae0df3c9b4aa594df1f54bb96f63c5ab6cb5765e
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-15 14:42:21 +1100

    regenerate css

--- niamoto_portal/settings/heroku.py
+++ niamoto_portal/settings/heroku.py
@@ -6,9 +6,9 @@ import dj_database_url
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 # Activate Django-Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
