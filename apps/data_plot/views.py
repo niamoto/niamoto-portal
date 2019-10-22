0cf456907da880252ab9bdcb463407a2079571cf
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-22 13:22:58 +1100

    add view dashbord plot view

--- apps/data_plot/views.py
+++ apps/data_plot/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.template import loader
+from .models import Graph
 
 # Create your views here.
+
+
+class DashboardPlotView(TemplateView):
+    """
+    Class-based view for plot dashboards page.
+    """
+    template_name = 'data_plot/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        graph_list = Graph.objects.filter(show=True).order_by('sort')
+        return {
+            'graph_list': graph_list,
+        }
