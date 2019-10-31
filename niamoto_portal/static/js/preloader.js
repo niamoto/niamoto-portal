ffcad6f5c4a8c05cb718d2b114788e157f10dfb9
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-31 11:44:25 +1100

    add svg preloader

---
+++ niamoto_portal/static/js/preloader.js
@@ -0,0 +1,7 @@
+export function showPreloader () {
+  document.getElementById('preloader').style.display = 'inline'
+}
+
+export function hidePreloader () {
+  document.getElementById('preloader').style.display = 'none'
+}
