71b5860a98a51384385f25fa1e351afa32770b18
Tristan Mangeard <tmangeard@gmx.fr>
2019-11-04 08:13:53 +1100

    add taxin admin

--- apps/data_taxon/admin.py
+++ apps/data_taxon/admin.py
@@ -9,3 +9,11 @@ from .models import Taxon, Frequency, Graph
 class GraphAdmin(admin.ModelAdmin):
     list_display = ('label', 'show', 'sort', 'title', 'model', 'height')
     list_editable = ('show', 'sort', 'title', 'model', 'height')
+
+
+@admin.register(Taxon)
+class TaxonAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'rank_name', 'parent',
+                    'id_endemia', 'id_rang', 'occ_count', 'plot_count')
+    list_editable = ('rank_name', 'parent',
+                     'id_endemia', 'id_rang', 'occ_count', 'plot_count')
