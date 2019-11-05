f3b7d1158874928e7020474e0cf96d3723636264
Tristan Mangeard <tmangeard@gmx.fr>
2019-11-05 13:06:47 +1100

    cached url dashboard plot

--- apps/data_plot/urls.py
+++ apps/data_plot/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 from . import views
 
 app_name = 'plot'
 
 urlpatterns = [
-    path('donnees/', views.DashboardPlotView.as_view(), name='dashboard'),
+    path('donnees/', cache_page(None)
+         (views.DashboardPlotView.as_view()), name='dashboard'),
     path('', views.PresentationPlotView.as_view(), name='presentation')
 ]
