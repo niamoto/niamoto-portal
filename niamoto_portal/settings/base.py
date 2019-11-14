c618f062776eea20305302811cf50fd01393551c
TM <tmangeard@gmx.fr>
2019-11-15 10:58:49 +1100

    add static_storage

--- niamoto_portal/settings/base.py
+++ niamoto_portal/settings/base.py
@@ -57,12 +57,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'niamoto_portal.urls'
@@ -182,11 +184,11 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
+    # STATICFILES_DIRS = [
+    #     os.path.join(BASE_DIR, 'static'),
+    # ]
 
 # Parameter for DEBUG
 if DEBUG:
