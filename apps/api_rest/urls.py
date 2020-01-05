1620db7f0acd6ed04642453229b70b9f72bc51d7
TM <tmangeard@gmx.fr>
2020-01-06 08:40:24 +1100

    change name base_name to basename

--- apps/api_rest/urls.py
+++ apps/api_rest/urls.py
@@ -3,12 +3,12 @@ from rest_framework import routers
 from . import views
 
 router = routers.SimpleRouter()
-router.register(r'Plot', views.PlotsViewSet, base_name='plot')
-router.register(r'Shape', views.ShapesViewSet, base_name='shape')
+router.register(r'Plot', views.PlotsViewSet, basename='plot')
+router.register(r'Shape', views.ShapesViewSet, basename='shape')
 router.register(r'ShapeLocation', views.ShapeLocationViewSet,
-                base_name='shapeLocation')
-router.register(r'Taxon', views.taxonsViewSet, base_name='taxon')
-router.register(r'TaxonTree', views.taxonsTreeViewSet, base_name='taxon_tree')
+                basename='shapeLocation')
+router.register(r'Taxon', views.taxonsViewSet, basename='taxon')
+router.register(r'TaxonTree', views.taxonsTreeViewSet, basename='taxon_tree')
 
 urlpatterns = [
     path('', include(router.urls)),
