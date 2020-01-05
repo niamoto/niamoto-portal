ae353b3e28344b5a470b96f9115ed2dabc4a6fb4
TM <tmangeard@gmx.fr>
2020-01-06 10:01:57 +1100

    add test taxon name

--- apps/data_taxon/tests.py
+++ apps/data_taxon/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 # Create your tests here.
 from django.urls import reverse
-from .models import Graph
+from .models import Graph, Taxon, Phenology, Frequency
 
 
 class PageTestCase(TestCase):
@@ -22,3 +22,14 @@ class GraphTestCase(TestCase):
     def test_graph_get_name(self):
         graph1 = Graph.objects.get(label='graph1')
         self.assertEqual(str(graph1), 'graph1')
+
+
+class TaxonTestCase(TestCase):
+
+    def setUp(self):
+        Taxon.objects.create(full_name='taxon1', rank_name='family',
+                             id_rang=1)
+
+    def test_taxon_get_name(self):
+        taxon1 = Taxon.objects.get(full_name='taxon1')
+        self.assertEqual(str(taxon1), 'taxon1')
