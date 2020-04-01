9b220eecf90d72fb264a3d3578b131a640e5a7b4
TM <tmangeard@gmx.fr>
2020-04-01 11:21:33 +1100

    change host for db postgres

--- niamoto_portal/settings/production.py
+++ niamoto_portal/settings/production.py
@@ -13,3 +13,17 @@ MIDDLEWARE = [
 ]
 # Enable GZip.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        'OPTIONS': {
+            'options': '-c search_path=niamoto_portal,public'
+        },
+        'HOST': 'niamoto-postgres',
+        'PORT': '5432',
+        'NAME': 'niamoto',
+        'USER': 'niamoto',
+        'PASSWORD': 'niamoto',
+    },
+}
