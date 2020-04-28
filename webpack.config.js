e2417b7a8847fcbd0fd5996ae275520427bdcdbd
TM <tmangeard@gmx.fr>
2020-04-28 16:57:52 +1100

    comment apps not use

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   mode: "production",
   entry: {
     "/data_shape/static/shape/js/": "./niamoto_portal/static/js/shape/main.js",
-    "/data_plot/static/plot/js/": "./niamoto_portal/static/js/plot/main.js",
-    "/data_taxon/static/taxon/js/": "./niamoto_portal/static/js/taxon/main.js",
+    // "/data_plot/static/plot/js/": "./niamoto_portal/static/js/plot/main.js",
+    // "/data_taxon/static/taxon/js/": "./niamoto_portal/static/js/taxon/main.js",
   },
   output: {
     filename: "[name]main.min.js",
