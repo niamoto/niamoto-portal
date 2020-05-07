f27c9bee42c19a812958fbec82b3120ef329ac18
TM <tmangeard@gmx.fr>
2020-05-07 14:13:09 +1100

    move media file

--- niamoto_portal/settings/base.py
+++ niamoto_portal/settings/base.py
@@ -175,7 +175,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media')
 
 if os.environ.get('ENV') == 'PRODUCTION':
 
