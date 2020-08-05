590350c62fc71563996cf8a1736f1765bc2bef1c
TM <tmangeard@gmx.fr>
2020-08-05 14:38:23 +1100

    add json general infos taxon

--- apps/data_taxon/urls.py
+++ apps/data_taxon/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
 
 from . import views
 
 app_name = 'taxon'
 
+router = routers.SimpleRouter()
+router.register(r'generalInfos', views.GeneralInfosViewSet, basename='generalInfos'
+                )
+
 urlpatterns = [
     path('donnees/', views.DashboardTaxonView.as_view(), name='dashboard'),
-    path('', views.PresentationTaxonView.as_view(), name='presentation')
+    path('', views.PresentationTaxonView.as_view(), name='presentation'),
+    path('', include(router.urls))
 ]
--- apps/data_taxon/views.py
+++ apps/data_taxon/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
 from django.http import HttpResponse
 from django.template import loader
-from .models import Graph
-
+from .models import Graph, Taxon
+from django.db.models import Max, Min, Avg, Count, Sum
 # Create your views here.
 
 
@@ -23,3 +27,33 @@ class DashboardTaxonView(TemplateView):
 class PresentationTaxonView(TemplateView):
 
     template_name = 'data_taxon/presentation.html'
+
+
+class GeneralInfosViewSet(ViewSet):
+    """
+    Viewset providing taxons general info .
+    """
+    @method_decorator(cache_page(60*60*24*300))
+    def retrieve(self, request, pk=None):
+
+        response = {
+            'ncpippn_count_sum': list(Taxon.objects.filter(id_rang=10).aggregate(Sum('ncpippn_count')).values())[0],
+            'ncpippn_count_max': list(Taxon.objects.filter(id_rang=10).aggregate(Max('ncpippn_count')).values())[0],
+            'dbh_max':  list(Taxon.objects.all().aggregate(Max('dbh_max')).values())[0],
+            'height_max':  list(Taxon.objects.all().aggregate(Max('height_max')).values())[0],
+            'wood_density_max':  list(Taxon.objects.all().aggregate(Max('wood_density_max')).values())[0],
+            'bark_thickness_max':  list(Taxon.objects.all().aggregate(Max('bark_thickness_max')).values())[0],
+            'leaf_sla_max':  list(Taxon.objects.all().aggregate(Max('leaf_sla_max')).values())[0],
+            'leaf_area_max':  list(Taxon.objects.all().aggregate(Max('leaf_area_max')).values())[0],
+            'leaf_thickness_max':  list(Taxon.objects.all().aggregate(Max('leaf_thickness_max')).values())[0],
+            'leaf_ldmc_max':  list(Taxon.objects.all().aggregate(Max('leaf_ldmc_max')).values())[0],
+            'freq_max':  list(Taxon.objects.all().aggregate(Max('freq_max')).values())[0],
+            'freq10_max':  list(Taxon.objects.all().aggregate(Max('freq10_max')).values())[0],
+            'freq_plot1ha_max':  list(Taxon.objects.all().aggregate(Max('freq_plot1ha_max')).values())[0],
+
+        }
+
+        return Response(response)
+
+    def list(self, request):
+        return Response({})
